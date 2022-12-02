ebabdc550154b4fa96bfdc641bf76d0b544f66ed
Sascha <42777981+SaschaCoffee@users.noreply.github.com>
2022-12-02 13:24:37 +0100

    google und firebase gehen

---
+++ app/src/main/java/com/example/firebasefeedtest/facebookSignModel.java
@@ -0,0 +1,26 @@
+package com.example.firebasefeedtest;
+
+public class facebookSignModel {
+    String name,email;
+
+    public facebookSignModel(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
